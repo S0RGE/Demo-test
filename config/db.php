8355e046e938cec2251399395065a773da31a3f9
NifontovVitaliy <vitaliynif@gmail.com>
2022-05-10 21:34:51 +0700

    test

---
+++ config/db.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=tiyijasa_m1',
+    'username' => 'tiyijasa',
+    'password' => 'DtR8TE',
+    'charset' => 'utf8',
+
+    // Schema cache options (for production environment)
+    //'enableSchemaCache' => true,
+    //'schemaCacheDuration' => 60,
+    //'schemaCache' => 'cache',
+];
